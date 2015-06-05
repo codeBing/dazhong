026768e3e743997373fc03ee74fb7f6a2257a3d6
BING <407272759@qq.com>
2015-06-05 16:11:34 +0800

    Revert "大众点评团购评论爬取"
    This reverts commit 3578da7fe533b58c656467afd6c471d619c51589.

--- user_agents.py
+++
@@ -1,6 +0,0 @@
-#-*- coding:utf-8 -*- 
-__author__ = 'BING'
-
-user_agents = [
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
-]
